8cc0a528e23d4496072946c6250cf4a06a87f5b8
michaeloye <michaeloye23@gmail.com>
2025-03-31 00:31:28 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# monitor soil moisture
+import RPi.GPIO from GPIO
+import time
+import Adafruit_DHT
+
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+def moisture_callback(channel):
+  if GPIO.input(channel):
+    print("no water detected")
+    # activate water pump
+  else:
+    print("water detected")
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, moisture_callback)
+
+# while True:
+#   time.sleep(1)
+
+# monitor temperature
+channel = 4
+SENSOR = Adafruit_DHT.DHT11
+
+def temp_callback(channel):
+  humidity, temperature = Adafruit_DHT.read_retry(SENSOR, channel)
+
+  if humidity is not None and temperature is not None:
+    print(f"Temperature is {temperature:1f}C")
+    print(f"Humidity is {humidity:1f}%")
+  else:
+    print("Failed to retrieve data from the sensor")
+
+# while True:
+#   time.sleep(1)
+
+
+# monitor humidity
